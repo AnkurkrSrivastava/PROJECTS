b25d2d7c2427d829528f8c963bc80c23548f8c10
AnkurkrSrivastava <ankur821305@gmail.com>
2025-08-04 23:24:36 +0530

    More styling and JS

--- Todo/todo.js
+++ Todo/todo.js
@@ -1,11 +1,29 @@
-add = document.getElementById("11");
-add.addEventListener("Add"),()=>{
-    console.log("updating list..");
-    tit = document.getElementById("TodoTitle").value
-    desc = document.getElementById("TodoDescription").value
-    if (localStorage.getItem('itemsJson')) {
-        itemJsonArray = [];
-        itemJsonArray.push([tit,desc]);
-        localStorage.setItem('itemJson',JSON.stringify(itemJsonArray))
+let btn1 = document.querySelector("#addBtn");
+let btn2 = document.querySelector("#clearAll");
+let btn3 = document.querySelector("#DelTodo");
+// btn1.onclick = () => {
+//     console.log("Add button was clicked");
+// }
+// btn2.onclick = () => {
+//     console.log("Clear All button was cliked");
+// }
+btn1.addEventListener("click", () => {
+    console.log("Updating list..");
+    let tit = document.getElementById("TodoTitle").value;
+    let desc = document.getElementById("TodoDescription").value;
+    let itemsJsonArray = [];
+    if(localStorage.getItem('itemsJson')){
+        itemsJsonArray.JSON.parse(localStorage.getItem('itemsJson'));
     }
-};
+
+    itemsJsonArray.push([tit,desc]);
+
+    localStorage.setItem('itemJson',JSON.stringify(itemsJsonArray));
+
+    console.log("Todo added:", [tit,desc]);
+});
+
+btn2.addEventListener("click", () => {
+    console.log("Clearing list..");
+    
+})
